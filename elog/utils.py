83cdccfc114098fac9c678c912af2f7ed61c6801
Tyler Pennebaker <tntp@stanford.edu>
2021-04-16 09:57:03 -0700

    ENH: Fix instrument log names for L2SI hutches

--- elog/utils.py
+++ elog/utils.py
@@ -5,4 +5,6 @@ Utility functions for PCDS ELog
 
 def facility_name(hutch):
     """Return the facility name for an instrument"""
+    if hutch in ['tmo', 'rix', 'txi', 'TMO', 'RIX', 'TXI']:
+        return '{}_Instrument'.format(hutch.lower())
     return '{}_Instrument'.format(hutch.upper())
