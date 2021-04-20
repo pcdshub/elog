6291c3def0a9ff6d1de9dd9ee2ff332969516c15
Zachary Lentz <zlentz@slac.stanford.edu>
2021-04-20 11:21:40 -0700

    TST: add tests for elog registry

--- tests/test_elog.py
+++ tests/test_elog.py
@@ -2,11 +2,12 @@ import os
 import pytest
 
 import elog.elog
+from elog.elog import HutchELog
+from elog.utils import clear_registry, registry
 
 
 @pytest.fixture(scope='module')
-def mockelog():
-
+def patch_webservice():
     # A very basic mock Webservice
     class WebService:
 
@@ -25,8 +26,13 @@ def mockelog():
     # Store value
     orig_web = elog.elog.PHPWebService
     elog.elog.PHPWebService = WebService
-    yield elog.elog.HutchELog('TST')
+    yield
     elog.elog.PHPWebService = orig_web
+
+
+@pytest.fixture(scope='module')
+def mockelog(patch_webservice):
+    yield HutchELog('TST')
 
 
 @pytest.fixture(scope='function')
@@ -64,7 +70,7 @@ def test_elog_post(mockelog):
 
 def test_elog_from_conf(temporary_config):
     # Fake ELog that stores the username and pw internally
-    class TestELog(elog.elog.HutchELog):
+    class TestELog(HutchELog):
 
         def __init__(self, instrument, user=None, pw=None):
             self.instrument = instrument
@@ -75,3 +81,20 @@ def test_elog_from_conf(temporary_config):
     assert log.instrument == 'TST'
     assert log.user == 'user'
     assert log.pw == 'pw'
+
+
+def test_elog_from_registry(patch_webservice):
+    clear_registry()
+    with pytest.raises(ValueError):
+        HutchELog.from_registry()
+    elog0 = HutchELog('TST', primary=False)
+    with pytest.raises(ValueError):
+        HutchELog.from_registry()
+    elog1 = HutchELog('TST')
+    assert HutchELog.from_registry() is elog1
+    elog2 = HutchELog('TST')
+    assert HutchELog.from_registry() is elog1
+    elog3 = HutchELog('TST', primary=True)
+    assert HutchELog.from_registry() is elog3
+    for elog in (elog0, elog1, elog2, elog3):
+        assert elog in registry
