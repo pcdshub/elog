457d999a876b25cd7f54bd1afb81943973c67fea
tangkong <tangkongrobert@gmail.com>
2022-02-15 16:39:50 -0800

    MAINT: clean up url handling

--- elog/pswww.py
+++ elog/pswww.py
@@ -40,11 +40,13 @@ class PHPWebService:
         self._auth = None
         self._base_url = base_url if base_url else self.base_url
         self.url = None
-        if dev:
-            self._lgbk = 'devlgbk'
-        else:
-            self._lgbk = 'lgbk'
+
         self.authenticate(user=user, pw=pw)
+
+        if dev:
+            self._lgbk_base_url = self._url + '/devlgbk'
+        else:
+            self._lgbk_base_url = self._url + '/lgbk'
 
     def authenticate(self, user=None, pw=None):
         """
@@ -111,7 +113,7 @@ class PHPWebService:
             web.get_facilities_logbook('CXI Instrument')
         """
         # Format correct URL
-        url = (self._url + f'/{self._lgbk}/lgbk/' + instrument + '/ws/info')
+        url = (self._lgbk_base_url + '/lgbk/' + instrument + '/ws/info')
         # Make request to WebService
         result = requests.get(url, **self._auth)
         # Invalid HTTP code
@@ -156,9 +158,11 @@ class PHPWebService:
 
         logger.debug("Requesting current experiment for %s", instrument)
         # Format correct URL
-        url = '{url}/{lgbk}/lgbk/ws/activeexperiment_for_instrument_station' \
-              '?instrument_name={instrument}' \
-              ''.format(url=self._url, lgbk=self._lgbk, instrument=instrument)
+        url = (
+            f'{self._lgbk_base_url}/lgbk/ws/'
+            'activeexperiment_for_instrument_station'
+            f'?instrument_name={instrument}'
+        )
         if station:
             url += '&station={}'.format(station)
         # Make request to WebService
@@ -230,8 +234,8 @@ class PHPWebService:
                     mimetypes.guess_type(filename)[0])))
 
         # Make request to web service
-        url = self._url + f"/{self._lgbk}/lgbk/" + \
-            logbook_id + "/ws/new_elog_entry"
+        url = self._lgbk_base_url + "/lgbk/" \
+            + logbook_id + "/ws/new_elog_entry"
         if files:
             result = requests.post(url, data=post, files=files, **self._auth)
         else:
